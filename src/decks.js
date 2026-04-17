05b637add9363fdf36296a0d9a922cda002252a8
Thomas McLean <tmclean1403@gmail.com>
2026-04-17 13:36:15 -0400

    Deck seed data created, Main configured

---
+++ src/decks.js
@@ -0,0 +1,6 @@
+export const seed = [
+    { id: 1, Commander: "Teval, the Balanced Scale", colours: [ "Green", "Black", "Blue" ], bracket: 4, wins: 0, losses: 0, decklist: "https://moxfield.com/decks/lPHJAMqcGEmXdL31tJZGpw" },
+    { id: 2, Commander: "Jolene the Plunder Queen", colours: [ "Green", "Red" ], bracket: 3, wins: 0, losses: 0, decklist: "https://moxfield.com/decks/vKfyADqzGUicxQR2OMhLWg" },
+    { id: 3, Commander: "Inspirt, Flagship Vessel", colours: [ "Red", "Blue", "White" ], bracket: 2, wins: 0, losses: 0, decklist: "https://moxfield.com/decks/32L2ONqE4kelGFLDrc_5hg" },
+    { id: 4, Commander: "Hearthhull, the Worldseed", colours: [ "Green", "Black", "Red" ], bracket: 2, wins: 0, losses: 0, decklist: "https://moxfield.com/decks/fNQ51vcPw06vCZmtf1yuCA" }
+];
