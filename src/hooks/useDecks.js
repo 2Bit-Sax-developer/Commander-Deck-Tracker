147ea3bc0f20657eec1daa697e67ead7d67c995c
Thomas McLean <tmclean1403@gmail.com>
2026-04-21 18:00:39 -0400

    Further Index Tweak and hooks

--- src/hooks/useDecks.js
+++ src/hooks/useDecks.js
@@ -4,6 +4,31 @@ import { DECKS } from '../decks';
 const STORAGE_KEY = '2bit_deck_tracker';
 
 export default function useDecks() {
-    
+
+    const [decks, setDecks] = useState([]);
+
+    useEffect(() => {
+    try {
+      const raw = localStorage.getItem(STORAGE_KEY);
+
+      /*
+      console.log(raw);
+      console.log(!raw);
+      */
+
+      if(!raw){
+
+        setDecks(DECKS);
+
+      } else {
+
+        const data = JSON.parse(raw);
+        Array.isArray(data) ? setDecks(data) : setDecks([]);
+
+      }
+    } catch{
+      setDecks([]);
+    }
+  }, []);
 }
 
