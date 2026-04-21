15fb70d16a00b3945852fad33d0e6b51587c8cdf
Thomas McLean <tmclean1403@gmail.com>
2026-04-21 17:39:13 -0400

    Updated Index, 404 and Deck Values

--- src/decks.js
+++ src/decks.js
@@ -1,8 +1,8 @@
 export const DECKS = [
-    { id: 1, Commander: "Teval, the Balanced Scale", colours: [ "Green", "Black", "Blue" ], bracket: 4, wins: 0, losses: 0, decklist: "https://moxfield.com/decks/lPHJAMqcGEmXdL31tJZGpw" },
-    { id: 2, Commander: "Jolene the Plunder Queen", colours: [ "Green", "Red" ], bracket: 3, wins: 0, losses: 0, decklist: "https://moxfield.com/decks/vKfyADqzGUicxQR2OMhLWg" },
-    { id: 3, Commander: "Inspirt, Flagship Vessel", colours: [ "Red", "Blue", "White" ], bracket: 2, wins: 0, losses: 0, decklist: "https://moxfield.com/decks/32L2ONqE4kelGFLDrc_5hg" },
-    { id: 4, Commander: "Hearthhull, the Worldseed", colours: [ "Green", "Black", "Red" ], bracket: 2, wins: 0, losses: 0, decklist: "https://moxfield.com/decks/fNQ51vcPw06vCZmtf1yuCA" },
-    { id: 5, Commander: "Rootha, Mastering the Moment", colours: [ "Red", "Blue" ], bracket: 2, wins: 0, losses: 0, decklist:"" },
-    { id: 6, Commander: "Quintorius, History Chaser", colours: [ "Red", "White" ], bracket: 2, wins: 0, losses: 0, decklist:"" }
+    { id: 1, Commander: "Teval, the Balanced Scale", colours: [ "Green", "Black", "Blue" ], bracket: 4, wins: 5, losses: 15, decklist: "https://moxfield.com/decks/lPHJAMqcGEmXdL31tJZGpw" },
+    { id: 2, Commander: "Jolene the Plunder Queen", colours: [ "Green", "Red" ], bracket: 3, wins: 7, losses: 4, decklist: "https://moxfield.com/decks/vKfyADqzGUicxQR2OMhLWg" },
+    { id: 3, Commander: "Inspirt, Flagship Vessel", colours: [ "Red", "Blue", "White" ], bracket: 2, wins: 2, losses: 8, decklist: "https://moxfield.com/decks/32L2ONqE4kelGFLDrc_5hg" },
+    { id: 4, Commander: "Hearthhull, the Worldseed", colours: [ "Green", "Black", "Red" ], bracket: 2, wins: 6, losses: 5, decklist: "https://moxfield.com/decks/fNQ51vcPw06vCZmtf1yuCA" },
+    { id: 5, Commander: "Rootha, Mastering the Moment", colours: [ "Red", "Blue" ], bracket: 2, wins: 0, losses: 1, decklist: "https://moxfield.com/decks/fYcMwVq0z0CuV-5u7MhmzA" },
+    { id: 6, Commander: "Quintorius, History Chaser", colours: [ "Red", "White" ], bracket: 2, wins: 0, losses: 0, decklist: "https://moxfield.com/decks/zZSn5VCulk-LtKI5_YxGlQ" }
 ];
