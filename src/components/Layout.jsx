7d5c32ade116ec5d02fd79029c1e119b4653da5f
Thomas McLean <tmclean1403@gmail.com>
2026-04-21 18:09:51 -0400

    ran npm build

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,6 +1,6 @@
 import React, {useState, useMemo} from 'react'
 import {Link, Outlet} from 'react-router-dom'
-import {DECKS} from '../decks'
+//import {DECKS} from '../decks'
 import DeckContext from './deckContext'
 
 export default function Layout(){
